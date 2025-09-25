b7e03fce13413ec65ce340a891b63333b0f9035f
ArtGev21 <arturgevorgyan2011@gmail.com>
2025-09-25 14:04:58 -0700

    Fix ESLint dependency conflict

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"KK4UrkdETc8hoUJX+Q7koB0C/r9+PhMP2lvMg2caTyA=\"\n}"
